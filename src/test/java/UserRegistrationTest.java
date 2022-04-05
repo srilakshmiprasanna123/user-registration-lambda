d7edc5b321d35d4b63c7f2d581cc911ae0865c7d
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-06 00:44:56 +0530

    added last name

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+
+
 public class UserRegistrationTest {
 
     UserRegistration userRegistration = new UserRegistration();
@@ -8,15 +10,27 @@ public class UserRegistrationTest {
 
     @Test
     public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
-        boolean result = userRegistration.firstName("Sandhya");
+        boolean result = userRegistration.firstName("Sri");
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
-        boolean result = userRegistration.firstName("Sandhya");
+        boolean result = userRegistration.firstName("Sri");
         Assert.assertEquals(false, result);
     }
 
+    @Test
+    public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.lastName("Lucky");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+        boolean result = userRegistration.lastName("lucky");
+        Assert.assertEquals(false, result);
+
+    }
 
 }
